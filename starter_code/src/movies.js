271bae4b35423a5f3da8eaa0ac34706583f1c977
Sam Vargas <sam.vargas@gmx.com>
2019-10-24 14:45:20 +0100

    Iteration2 complete

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,7 +2,28 @@
 
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals
 
+function calculateAverageMovieRate(arrayOfMovies){
+    const sumOfRatings = arrayOfMovies.reduce((accumulator, value, index, originalArray) => {
+        return accumulator + arrayOfMovies[index].rate;
+    }, 0);
+    return sumOfRatings / arrayOfMovies.length;
+    // return Number((sumOfRatings / arrayOfMovies.length).toFixed(2));
+    // return Math.round(((sumOfRatings / arrayOfMovies.length)*100)/100);
+}
+
+
 // Iteration 2: Drama movies - Get the average of Drama Movies
+
+function calculateAverageDramaRate(arrayOfMovies){
+    const dramaArray = arrayOfMovies.filter((movie, index) => {
+        return (arrayOfMovies[index].genre.includes('Drama'));
+    });
+    if (Number(dramaArray) === 0){
+        return 0;
+    } else {
+      return calculateAverageMovieRate(dramaArray); 
+    }
+}
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
